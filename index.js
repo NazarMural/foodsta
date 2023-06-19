f3fad901538b80ee5437f9466c57179c5366d1b5
NazarMural <nazarmural777@gmail.com>
2023-06-19 20:01:00 +0300

    Додає мобільну версію героя
    Та частково додає ще секцію offering

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const menuContainer = document.querySelector('.menu-container');
 menuButton.addEventListener('click', () => {
   menuContainer.classList.toggle('open');
 });
+
+document.addEventListener('click', event => {
+  const target = event.target;
+  const isMenuOpen = menuContainer.classList.contains('open');
+  const isInsideMenu = menuContainer.contains(target);
+  const isMenuButton = target.closest('.menu-button__icon');
+
+  if (isMenuOpen && !isInsideMenu && !isMenuButton) {
+    menuContainer.classList.remove('open');
+  }
+});
